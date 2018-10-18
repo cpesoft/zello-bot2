aaafb537d0e067be38981cd2590d77414b8436e3
Carsten Perthel <carsten@cpesoft.com>
2018-10-18 13:36:17 +0200

    Boilerplate project config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # zello-bot2
+Use the Zello Channel API to do some bot-like tasks.
+
+"Cuckoo Clock, Single, A.wav" by InspectorJ of Freesound.org
+
+TTS Engine: www.fromtexttospeech.com
+
+The Zello name, associated trademarks and logos are trademarks of Zello Inc.
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  // entry is where, say, your app starts - it can be called main.ts, index.ts, app.ts, whatever
+  entry: ['./src/index.ts'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'zellobot2.bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.jsx', '.json']
+  },
+  plugins: [
+      new CleanWebpackPlugin(['dist'])
+  ]
+};
